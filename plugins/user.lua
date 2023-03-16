a731df96725bcf601a3a9df6b5199b66830f0e86
Johannes Haapanen <johannes.haapanen@gmail.com>
2023-03-16 16:59:10 +0200

    Basic plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,25 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  --
+  { "opdavies/toggle-checkbox.nvim" },
+  { "ellisonleao/glow.nvim" },
+  {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end,
+  },
+  { "lervag/vimtex" },
+  -- You can disable default plugins as follows:
+  -- ["goolord/alpha-nvim"] = { disable = true },
+  ["nvim-lua/plenary.nvim"] = { disabled = true },
+  -- You can also add new plugins here as well:
+  -- Add plugins, the packer syntax without the "use"
+  { "ellisonleao/gruvbox.nvim" },
 }
